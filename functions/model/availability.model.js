d09ca040f3f599ccacd811687d777dbaa419970c
Murtadha Alobaidi <mhaao@kth.se>
2023-02-03 21:18:06 +0100

    Update availability.model.js
    Co-Authored-By: Abdullah Trabulsiah <83720452+Abdullahtr249@users.noreply.github.com>

--- functions/model/availability.model.js
+++ functions/model/availability.model.js
@@ -3,3 +3,34 @@ const Sequelize = require('sequelize') // ORM for connection with postgres
 const validator = require('validator') // Framework for string validation
 const bcrypt = require('bcrypt') // Library for encrypting data
 const { db } = require('../db') // Connection to database
+
+const Availability = db.define(
+  'availability',
+  {
+    availability_id: {
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    pers_id: {
+      type: Sequelize.INTEGER,
+      required: true,
+    },
+    from_date: {
+      type: date,
+      required: true,
+    },
+    to_date: {
+      type: date,
+      required: true,
+    },
+  },
+  {
+    tableName: 'availability',
+    timestamps: false,
+  },
+)
+
+// Synchronize the model with the database
+db.sync({ force: false })
+
+module.exports = Availability
