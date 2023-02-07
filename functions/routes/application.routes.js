969f734e86bd03f28bd764b38e35253c861604cc
Murtadha Alobaidi <mhaao@kth.se>
2023-02-08 00:11:47 +0100

    write to db
    Co-Authored-By: Abdullah Trabulsiah <83720452+Abdullahtr249@users.noreply.github.com>
    
    #14 add not finish , we need  help ;(

--- functions/routes/application.routes.js
+++ functions/routes/application.routes.js
@@ -1,24 +1,72 @@
-/* eslint-disable */
-const {Router} = require("express");
-const authenticated = require("../middleware/auth.middleware");
+const express = require('express')
+const router = express.Router()
+const authenticated = require('../middleware/auth.middleware')
+const { check, validationResult } = require('express-validator')
+const { formErrorFormatter } = require('../util/errorFormatter')
+const _ = require('lodash')
+const jwt = require('jsonwebtoken')
 
-const router = Router();
+const { registerAvailability } = require('../controller/application.controller')
 
-router
+/*Application List*/
+router.get('/applications', authenticated, (req, res) => {
+  res.render('applications', {
+    user: req.user,
+    error: req.flash('error'),
+    form_error: req.flash('form-error'),
+  })
+})
 
-    /*Application List*/
-    .get("/applications", authenticated, (req, res) => { 
-        
-        res.render('applications', {
-            user: req.user
-        }); 
-    })
+/*Application-form*/
+router.get('/application-form', authenticated, (req, res, next) => {
+  res.render('application-form', {
+    user: req.user,
+    error: req.flash('error'),
+    form_error: req.flash('form-error'),
+  })
+})
 
-    /*Application-form*/
-    .get("/application-form", authenticated, (req, res) => { 
-        res.render('application-form', {
-             user: req.user
-        }); 
-    })
+router.post(
+  '/application-form',
+  [
+    check(
+      'from_date',
+      'You have to enter the start date of your availability period',
+    ).exists(),
+    check(
+      'to_date',
+      'You have to enter the end date of your availability period',
+    ).exists(),
+  ],
+  (req, res) => {
+    const { from_date, to_date } = _.pick(req.body, ['from_date', 'to_date'])
 
-module.exports = router;
+    // Form errors
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      req.flash('form-error', formErrorFormatter(errors))
+      return res.redirect(
+        '/iv1201-recruitmenapp/us-central1/app/application/application-form',
+      )
+    }
+
+    registerAvailability(from_date, to_date)
+      .then((availability) => {
+        if (availability) {
+          const token = jwt.sign(
+            availability.availability_id.toString(),
+            process.env.JWT_TOKEN,
+          )
+          return res.redirect('/')
+        }
+      })
+      .catch((error) => {
+        req.flash('error', error)
+        return res.redirect(
+          '/iv1201-recruitmenapp/us-central1/app/application/application-form',
+        )
+      })
+  },
+)
+
+module.exports = router
