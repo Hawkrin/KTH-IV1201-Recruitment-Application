61681459cacbef918fac0019f316474c378dca5e
Murtadha Alobaidi <mhaao@kth.se>
2023-02-08 14:59:21 +0100

    ok
    Co-Authored-By: Abdullah Trabulsiah <83720452+Abdullahtr249@users.noreply.github.com>

--- functions/middleware/dbConnect.middleware.js
+++
@@ -1,8 +0,0 @@
-const { db } = require('../db');
-
-const connectToDb = (req, res, next) => {
-    req.db = db;
-    next();
-};
-
-module.exports = connectToDb;
--- functions/model/competence_profile.model.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-const Sequelize = require('sequelize') // ORM for connection with postgres
-const validator = require('validator') // Framework for string validation
-const bcrypt = require('bcrypt') // Library for encrypting data
-const { db } = require('../db') // Connection to database
