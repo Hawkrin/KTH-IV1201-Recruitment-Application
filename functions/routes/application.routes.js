b9f86d783821741ed654364eccad82bda93db426
Murtadha Alobaidi <mhaao@kth.se>
2023-02-12 13:29:23 +0100

    competences error
    competences error message done. Now all applicant part Done ;)
    
    Co-Authored-By: Abdullah Trabulsiah <83720452+Abdullahtr249@users.noreply.github.com>

--- functions/routes/application.routes.js
+++ functions/routes/application.routes.js
@@ -33,6 +33,7 @@ router
       success: req.flash('success'),
       error: req.flash('error'),
       form_error: req.flash('form-error'),
+      form_error1: req.flash('form-error1'),
     })
   })
   .post(
@@ -94,29 +95,20 @@ router
       )
 
       const { competences1, competences2, competences3 } = req.body
-      if (competences1 == '1') {
-        check(
-          'competence_1_start_time',
-          'You have to enter the start date of your availability period for Lotteries',
-        )
-          .isDate()
-          .run(req),
-          check(
-            'competence_1_end_time',
-            'You have to enter the end date of your availability period for Lotteries',
-          )
-            .isDate()
-            .run(req)
+      if (competences1 === '1') {
+
+
         // Form errors
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-          req.flash('form-error', formErrorFormatter(errors))
+        const errors = competence_1_start_time && competence_1_end_time;
+        if (errors == false) {
+          req.flash('form-error1', "You have to enter the start and end date of your competences ")
           return res.redirect(
             '/iv1201-recruitmenapp/us-central1/app/application/application-form',
           )
         }
+
         registerCompetence(person_id, 1, years_of_experience_1)
-          .then((newCompetenceProfile) => {})
+          .then((newCompetenceProfile) => { })
           .catch((error) => {
             req.flash('error', error.message)
             return res.redirect(
@@ -126,28 +118,17 @@ router
       }
 
       if (competences2 === '2') {
-        check(
-          'competence_2_start_time',
-          'You have to enter the start date of your availability period for Lotteries',
-        )
-          .isDate()
-          .run(req)
-        check(
-          'competence_2_end_time',
-          'You have to enter the end date of your availability period for Lotteries',
-        )
-          .isDate()
-          .run(req)
+
         // Form errors
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-          req.flash('form-error', formErrorFormatter(errors))
+        const errors = competence_2_start_time && competence_2_end_time;
+        if (errors == false) {
+          req.flash('form-error1', "You have to enter the start and end date of your competences ")
           return res.redirect(
             '/iv1201-recruitmenapp/us-central1/app/application/application-form',
           )
         }
         registerCompetence(person_id, 2, years_of_experience_2)
-          .then((newCompetenceProfile) => {})
+          .then((newCompetenceProfile) => { })
           .catch((error) => {
             req.flash('error', error.message)
             return res.redirect(
@@ -157,28 +138,18 @@ router
       }
 
       if (competences3 === '3') {
-        check(
-          'competence_3_start_time',
-          'You have to enter the start date of your availability period for Lotteries',
-        )
-          .isDate()
-          .run(req)
-        check(
-          'competence_3_end_time',
-          'You have to enter the end date of your availability period for Lotteries',
-        )
-          .isDate()
-          .run(req)
+
         // Form errors
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-          req.flash('form-error', formErrorFormatter(errors))
+        const errors = competence_3_start_time && competence_3_end_time;
+        if (errors == false) {
+          req.flash('form-error1', "You have to enter the start and end date of your competences ")
           return res.redirect(
             '/iv1201-recruitmenapp/us-central1/app/application/application-form',
           )
         }
+
         registerCompetence(person_id, 3, years_of_experience_3)
-          .then((newCompetenceProfile) => {})
+          .then((newCompetenceProfile) => { })
           .catch((error) => {
             req.flash('error', error.message)
             return res.redirect(
